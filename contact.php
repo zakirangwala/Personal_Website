e1ef906ac6ef110d872159b08931ea013a0294b7
Zaki Rangwala <zakirangwala@gmail.com>
2019-12-10 17:58:02 -0500

    contact form
    update

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 $from = 'Demo contact form <demo@domain.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'Demo contact form <zakirangwala@gmail.com>';
 
 // subject of the email
 $subject = 'New message from contact form';
