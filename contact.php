3b9c9419729ec14082583f3291bdef1211d4dcf5
Zaki Rangwala <zakirangwala@gmail.com>
2020-05-28 20:38:18 -0400

    done

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@ if (isset($_POST['submit'])) {
     $mailFrom = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
-    $mailTo = "zakirangwala@gmail.com";
+    $mailTo = "zakirangwala@outlook.com";
     $headers = "From: ".$mailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
     mail($mailTo, $subject, $txt, $headers);
