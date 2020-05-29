40de1c2bbc3031a5ec9d39ecd54f549e1e6dd4a0
Zaki Rangwala <zakirangwala@gmail.com>
2020-05-28 20:08:52 -0400

    sad
    asd

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'zakirangwala@gmail.com';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'zakirangwala@gmail.com';
 
 // subject of the email
 $subject = 'New message from contact form';
